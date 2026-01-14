aad96ffa5637af478b8d4dec7127bc0d3db961f5
jonathan_Stanford <jonathan.stanford@topgolf.com>
2026-01-14 20:33:43 +0100

    try 500

---
+++ PersonnummerValidator.cs
@@ -0,0 +1,10 @@
+namespace ConsoleApp_CICD_gruppuppgift
+{
+    public static class PersonnummerValidator
+    {
+        public static bool IsValid(string input)
+        {
+            return true;
+        }
+    }
+}
