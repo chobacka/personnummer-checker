0f3cfd1b17c47b1c0ffd4c3e915546ec02924ee4
ArashFP <Arash_afp_94@hotmail.com>
2026-01-15 18:39:56 +0100

    adds validation method, tweaks program.cs to work with new method aswell as yearTests.cs

--- PersonnummerValidator.cs
+++ PersonnummerValidator.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace ConsoleAppCICDgruppuppgift
 {
-    public static class PersonnummerValidator
+  public static class SocialSecurityNumberValidator
+  {
+    public static bool IsValidSocialSecurityNumber(string socialSecurityNumber)
     {
-        public static bool Validate(string pnr)
-        {
-            throw new NotImplementedException();
-        }
+      if (string.IsNullOrWhiteSpace(socialSecurityNumber))
+        return false;
+
+      // Expected format: YYMMDD-XXXX
+      if (!Regex.IsMatch(socialSecurityNumber, @"^\d{6}-\d{4}$"))
+        return false;
+
+      // Remove dash
+      var digits = socialSecurityNumber.Replace("-", "");
+
+      // Validate date part
+      var datePart = digits.Substring(0, 6);
+
+      if (!DateTime.TryParseExact(
+              datePart,
+              "yyMMdd",
+              CultureInfo.InvariantCulture,
+              DateTimeStyles.None,
+              out _))
+        return false;
+
+      // Luhn algorithm (first 9 digits)
+      int sum = 0;
+      for (int i = 0; i < 9; i++)
+      {
+        int digit = digits[i] - '0';
+        int temp = digit * (i % 2 == 0 ? 2 : 1);
+
+        if (temp > 9)
+          temp -= 9;
+
+        sum += temp;
+      }
+
+      int controlDigit = (10 - (sum % 10)) % 10;
+      int lastDigit = digits[9] - '0';
+
+      return controlDigit == lastDigit;
     }
+  }
 }
